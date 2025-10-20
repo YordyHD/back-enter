dec7cfe65904b69d6c30c759aeece0b934cae44e
yordyhd <alexanderyordy5gmail.com>
2025-10-20 17:16:09 -0500

    chore: primer commit

---
+++ src/test/java/com/sena/enter/EnterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.enter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
