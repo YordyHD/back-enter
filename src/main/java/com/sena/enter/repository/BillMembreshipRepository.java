f6f1528cd683c95abf20e27a53f60c503e747e4c
julian andres triana rincon <julianandrestriana81@gmail.com>
2025-10-28 17:09:53 -0500

    chore: se agrega repository BillMembreship

--- src/main/java/com/sena/enter/repository/BillMembreshipRepository.java
+++ src/main/java/com/sena/enter/repository/BillMembreshipRepository.java
@@ -1,8 +1,12 @@
 package com.sena.enter.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sena.enter.models.BillMembreship;
+import com.sena.enter.models.BillMembreshipId;
 
-public interface BillMembreshipRepository extends JpaRepository<BillMembreship, Long>{
-
+public interface BillMembreshipRepository extends JpaRepository<BillMembreship, BillMembreshipId>{
+    List<BillMembreship> findByBill_Id(Long billId);
+    List<BillMembreship> findByMembreship_Id(Long membreshipId);
 }
