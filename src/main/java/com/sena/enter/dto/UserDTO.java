a734c7d5d54fa097d536fd9163050c5898bf2846
Yordy Alarcon <alexanderyordy5@gmail.com>
2025-11-03 17:21:39 -0500

    se creo paquetes services y controllers

--- src/main/java/com/sena/enter/dto/UserDTO.java
+++ src/main/java/com/sena/enter/dto/UserDTO.java
@@ -3,7 +3,6 @@ package com.sena.enter.dto;
 import java.io.Serializable;
 import java.util.Set;
 import com.sena.enter.models.User;
-import com.sena.enter.models.Authority;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
