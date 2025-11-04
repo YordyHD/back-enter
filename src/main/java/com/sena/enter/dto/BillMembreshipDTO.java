8a28a4bd55b9ce413f95bab1af4117022256db03
Yordy Alarcon <alexanderyordy5@gmail.com>
2025-11-03 21:58:10 -0500

    chore: se arreglo un error de billmembreshipDTO

--- src/main/java/com/sena/enter/dto/BillMembreshipDTO.java
+++ src/main/java/com/sena/enter/dto/BillMembreshipDTO.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,30 +17,17 @@ import lombok.Setter;
 
 public class BillMembreshipDTO implements Serializable{
 
-    @NotNull(message = "El ID de la factura (bill) no puede ser nulo")
-    private Long billId;
-    private Long numberBill; 
+        private Long id;
 
-    @NotNull(message = "El ID de la membresía no puede ser nulo")
-    private Long membreshipId;
-    private String membershipName; 
-
-    @NotNull(message = "La fecha inicial es obligatoria")
-    private LocalDate inicialDate;
-
-    @NotNull(message = "La fecha final es obligatoria")
     private LocalDate finalDate;
+
+    private LocalDate inicialDate;
 
     private BigDecimal salePrice;
 
-    @Size(max = 500, message = "Las observaciones no pueden superar los 500 caracteres")
-    private String observaciones;
+    @NotNull
+    private BillDTO bill;
 
-    @Column(nullable = false)
-    private String estado; 
-
-    @Column(nullable = false, unique = true)
-    private String usuario; 
-    @Column(nullable = false)
-    private String clave;
+    @NotNull
+    private MembreshipDTO membreship;
 }
